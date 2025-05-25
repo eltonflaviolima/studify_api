798214da3bf77264058730b469b20af2a2609f2f
Elton <eltonflaviolima@gmail.com>
2025-05-25 01:29:53 -0300

    FEAT: Adicionar página de login com JWT teste e atualizar rota de autenticação

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework.authtoken.views import obtain_auth_token
-
+from rest_framework_simplejwt.views import token_obtain_pair 
 from apps.flashcard.views import (
     CardViewSet,
     CardsByDeckViewSet,
@@ -19,7 +18,7 @@ router.register("users", UserViewSet)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/auth/login/", obtain_auth_token, name="api_token_auth"),
+    path("api/auth/login/", token_obtain_pair, name="api_token_auth"),
     path('api/auth/register/', RegisterView.as_view(), name='register'),
     path("api/", include(router.urls)),
     path("api/users/<int:pk>/decks/", DecksByUserViewSet.as_view()),
