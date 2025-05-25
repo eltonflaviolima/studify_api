59c9f5f8f74e217b09b1a9a38d71c87581035791
Elton <eltonflaviolima@gmail.com>
2025-05-25 01:29:25 -0300

    FEAT: Adicionar formulário de registro teste JWT e lógica de autenticação no frontend

--- src/apps/flashcard/views.py
+++ src/apps/flashcard/views.py
@@ -1,4 +1,4 @@
-from rest_framework import viewsets, generics
+from rest_framework import status, viewsets, generics
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
